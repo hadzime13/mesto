2fa592dae1c94364ae9b0931ab40f223686713f4
Andrey Varlamov <hadzime@yandex.ru>
2020-05-24 16:15:28 +0300

    fixed some issues

--- pages/script.js
+++ pages/script.js
@@ -1,31 +1,39 @@
+// Переменные
+// для попапа
 const popup = document.querySelector('.popup');
+// для имени и профессии в профайле и их текста
 const profileName = document.querySelector('.profile__name');
 const profileNameText = profileName.textContent;
 const profileJob = document.querySelector('.profile__job');
 const profileJobText = profileJob.textContent;
+// для полей ввода в попапе 
 const inputName = document.querySelector('.popup__text_el_name'); 
 const inputJob = document.querySelector('.popup__text_el_job');
+// для кнопок попапа 
 const editButton = document.querySelector('.edit-btn');
 const closeButton = document.querySelector('.popup__closebtn');
+//для формы попапа
 const form = document.querySelector('.popup__container');
 
-
-function popupOpen() {
-popup.classList.add('popup_opened');
-inputName.value = profileNameText;
-inputJob.value = profileJobText;
+// Фукнкция открытия/закрытия Попапа
+function popupOpenClose() {
+  if (popup.classList.contains('popup_opened') {
+    popup.classList.remove('popup_opened');
+  }
+  else { 
+    popup.classList.add('popup_opened');
+    inputName.value = profileNameText;
+    inputJob.value = profileJobText;
+  }
 }
-
-function popupClose() {
-  popup.classList.remove('popup_opened');
-}
+// Функция сохранения значений, введенных в попап  
 function formSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = inputName.value;
+  profileName.I = inputName.value;
   profileJob.textContent = inputJob.value;
   popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
+editButton.addEventListener('click', popupOpenClose);
+closeButton.addEventListener('click', popupOpenClose);
 form.addEventListener('submit', formSubmit);
