7a581f4934c89980e8a3532c063f12b3bd2b1acd
Andrey Varlamov <hadzime@yandex.ru>
2020-06-02 22:32:27 +0300

    рефактор JS

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,8 @@
+
+// *** Попап редактирования профиля
+
 // Переменные
-// для попапа
+// для попапа 
 const popup = document.querySelector('.popup');
 // для имени и профессии в профиле
 const profileName = document.querySelector('.profile__name');
@@ -35,3 +38,51 @@ function formSubmit(evt) {
 editButton.addEventListener('click', popupOpenClose);
 closeButton.addEventListener('click', popupOpenClose);
 form.addEventListener('submit', formSubmit);
+
+//*** Загрузка/редактирование карточек мест
+
+// Переменные
+// для шаблона карточки и списка, куда добавляем карточки
+const cardTemplate = document.querySelector('.photo-element').content;
+const cardsArea = document.querySelector('photo-elements__list');
+// Первоначальный массив карточек
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+// Функция первоначальной загрузки карточек
+
+const addInitialCards = (cardsArray) => {
+  for (let elem of cardsArray) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.photo-elements__image').src = elem.link;
+    cardElement.querySelector('.photo-elements__image').alt = elem.name;
+    cardElement.querySelector('.photo-elements__name').textContent = elem.name;
+    cardsArea.append(cardElement);
+  };
+};
+
+addInitialCards(initialCards);
