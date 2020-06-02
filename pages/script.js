f2561b8bc0b20e2f205f767dbcd326a8084e9b54
Andrey Varlamov <hadzime@yandex.ru>
2020-06-02 21:21:48 +0300

    рефактор JS

--- pages/script.js
+++ pages/script.js
@@ -1,16 +1,16 @@
 // Переменные
 // для попапа
 const popup = document.querySelector('.popup');
-// для имени и профессии в профайле
+// для имени и профессии в профиле
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-// для полей ввода в попапе 
+// для полей ввода в попапе рекдактирования профиля
 const inputName = document.querySelector('.popup__text_el_name'); 
 const inputJob = document.querySelector('.popup__text_el_job');
-// для кнопок попапа 
+// для кнопок попапа редактирования профиля
 const editButton = document.querySelector('.profile__edit-btn');
 const closeButton = document.querySelector('.popup__close-btn');
-//для формы попапа
+//для формы попапа редактирования профиля
 const form = document.querySelector('.popup__container');
 
 // Фукнкция открытия/закрытия Попапа
@@ -20,21 +20,18 @@ function popupOpenClose() {
   }
   else { 
     popup.classList.add('popup_opened');
-// Переменные для текста имени и профессии в профайле
-    const profileNameText = profileName.textContent;
-    const profileJobText = profileJob.textContent;
-    inputName.value = profileNameText;
-    inputJob.value = profileJobText;
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
   }
 }
-// Функция сохранения значений, введенных в попап  
+// Функция сохранения значений, введенных в попап профиля  
 function formSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   popupOpenClose();
 }
-
+// "Слушатели" попапа профиля
 editButton.addEventListener('click', popupOpenClose);
 closeButton.addEventListener('click', popupOpenClose);
 form.addEventListener('submit', formSubmit);
