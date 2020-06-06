b315d47378b68e353a6ae60e7066542e6baff2ab
Andrey Varlamov <hadzime@yandex.ru>
2020-06-06 19:32:19 +0300

    Исправлены замечания и можно лучше

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
-// *** Попап редактирования профиля
+// *** Переменные
 
-// Переменные
+// Попап профиля
 // для попапа 
 const popupProfile = document.querySelector('.popup_profile');
 // для имени и профессии в профиле
@@ -16,14 +16,33 @@ const closeButton = popupProfile.querySelector('.popup__close-btn');
 //для формы попапа редактирования профиля
 const profileForm = popupProfile.querySelector('.popup__container');
 
-// Функция открытия/закрытия попапов
+// Попап добавления карточки 
+// для шаблона карточки и списка, куда добавляем карточки
+const cardTemplate = document.querySelector('.template-photo').content;
+const cardsArea = document.querySelector('.photo-elements__list');
 
+const popupAddCards = document.querySelector('.popup_cards');
+// для полей ввода в попапе добавления карточек
+const inputCardName = popupAddCards.querySelector('.popup__text_el_place');
+const inputCardLink = popupAddCards.querySelector('.popup__text_el_image-link');
+// для кнопок попапа добавления карточек
+const addButton = document.querySelector('.profile__add-btn');
+const cardsCloseButton = popupAddCards.querySelector('.popup__close-btn');
+//для формы попапа добавления карточек
+const cardAddForm = popupAddCards.querySelector('.popup__container');
+
+// Попап фото
+const popupPhoto = document.querySelector('.popup_photo');
+const photoCloseButton = popupPhoto.querySelector('.popup__close-btn');
+
+// *** Функции
+
+// Функция открытия/закрытия попапов
 const popupOpenClose = (element) => {
   element.classList.toggle('popup_opened');
 }
 
 // Функция заполнения полей попапа профиля
-
 const profileEdit = () => {
   popupOpenClose(popupProfile);
   inputName.value = profileName.textContent;
@@ -37,45 +56,7 @@ const profileFormSubmit = (evt) => {
   profileJob.textContent = inputJob.value;
   popupOpenClose(popupProfile);
 }
-// "Слушатели" попапа профиля
-editButton.addEventListener('click', profileEdit);
-closeButton.addEventListener('click', function () { popupOpenClose(popupProfile) });
-profileForm.addEventListener('submit', profileFormSubmit);
 
-// *** Загрузка/редактирование карточек мест
-
-// Переменные
-// для шаблона карточки и списка, куда добавляем карточки
-const cardTemplate = document.querySelector('.template-photo').content;
-const cardsArea = document.querySelector('.photo-elements__list');
-// Первоначальный массив карточек
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 // Функция создания карточки и слушателей в ней
 
 const createCard = (name, link) => {
@@ -94,25 +75,6 @@ const createCard = (name, link) => {
   return cardElement;
 };
 
-// Загружаем первые 6 карточек из массива
-initialCards.forEach(elem => {
-  cardsArea.prepend(createCard(elem.name, elem.link));
-});
-
-// // *** Попап добавления карточки 
-
-// Переменные
-
-const popupAddCards = document.querySelector('.popup_cards');
-// для полей ввода в попапе добавления карточек
-const inputCardName = popupAddCards.querySelector('.popup__text_el_place');
-const inputCardLink = popupAddCards.querySelector('.popup__text_el_image-link');
-// для кнопок попапа редактирования профиля
-const addButton = document.querySelector('.profile__add-btn');
-const cardsCloseButton = popupAddCards.querySelector('.popup__close-btn');
-//для формы попапа редактирования профиля
-const cardAddForm = popupAddCards.querySelector('.popup__container');
-
 // Функции добавления карточки пользователем
 
 const cardPopupOpen = () => {
@@ -127,18 +89,6 @@ const cardsFormSubmit = (evt) => {
   popupOpenClose(popupAddCards);
 };
 
-// Слушатели попапа добавления карточек
-
-addButton.addEventListener('click', cardPopupOpen);
-cardsCloseButton.addEventListener('click', function () { popupOpenClose(popupAddCards) });
-cardAddForm.addEventListener('submit', cardsFormSubmit);
-
-// *** Попап с фото
-// Переменные
-
-const popupPhoto = document.querySelector('.popup_photo');
-const photoCloseButton = popupPhoto.querySelector('.popup__close-btn');
-
 // Функция попапа с фото
 
 function photoPopupOpen(evt) {
@@ -149,5 +99,24 @@ function photoPopupOpen(evt) {
   popupOpenClose(popupPhoto);
 }
 
+// *** Обработчики событий
+
+// "Слушатели" попапа профиля
+editButton.addEventListener('click', profileEdit);
+closeButton.addEventListener('click', function () { popupOpenClose(popupProfile) });
+profileForm.addEventListener('submit', profileFormSubmit);
+
+
+// Слушатели попапа добавления карточек
+addButton.addEventListener('click', cardPopupOpen);
+cardsCloseButton.addEventListener('click', function () { popupOpenClose(popupAddCards) });
+cardAddForm.addEventListener('submit', cardsFormSubmit);
+
+
 // Слушатель закрытия попапа с фото
 photoCloseButton.addEventListener('click', function () { popupOpenClose(popupPhoto) });
+
+// Загружаем первые 6 карточек из массива
+initialCards.forEach(elem => {
+  cardsArea.prepend(createCard(elem.name, elem.link));
+});
