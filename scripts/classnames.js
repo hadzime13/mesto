301bb451f1f81a1d482a03c339100b84c78fba4d
Andrey Varlamov <hadzime@yandex.ru>
2020-06-10 20:20:53 +0300

    Сверстан html

---
+++ scripts/classnames.js
@@ -0,0 +1,9 @@
+const classnames = {
+    formSelector: '.popup',
+    inputSelector: '.popup__text',
+    submitButtonSelector: '.popup__save-btn',
+    inactiveButtonClass: '.popup__save-btn_disabled',
+    inputErrorClass: '.popup__text_type_error',
+    errorClass: 'popup__error_visible'
+ 
+};
